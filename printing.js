fd7f8fdc245302e0d0b539bdbced54b377d2e4ce
Mike <pythomike@gmail.com>
2017-09-11 19:10:22 +0000

    adding Exercise 1

---
+++ printing.js
@@ -0,0 +1,20 @@
+var https = require('https');                               
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+    console.log(data + "\n");
+    });
+
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
