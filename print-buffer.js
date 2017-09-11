61cfccaa41957807fa8445118dc5ea93e8058e1c
Mike <pythomike@gmail.com>
2017-09-11 19:56:53 +0000

    updating Exercise 1 & 2

--- print-buffer.js
+++ print-buffer.js
@@ -1,22 +1,25 @@
 const https = require('https');
 
-const requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step2.html'
-};
+function getAndPrintHTML () {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
 
-https.get(requestOptions, (response) => {
+  https.get(requestOptions, (response) => {
 
-  response.setEncoding('utf8');
-  let myBuffer = '';
-  response.on('data', (data) => {
-    console.log('Chunk Received. Length:', data.length);
-    myBuffer += data;
-    console.log(myBuffer);
+    response.setEncoding('utf8');
+    let myBuffer = '';
+    response.on('data', (data) => {
+      console.log('Chunk Received. Length:', data.length);
+      myBuffer += data;
+      console.log(myBuffer);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
   });
-
-  response.on('end', function() {
-    console.log('Response stream complete.');
-  });
-
-});
+}
+console.log(getAndPrintHTML())
--- printing.js
+++ printing.js
@@ -1,20 +1,22 @@
-var https = require('https');                               
+const https = require('https');                               
+function getAndPrintHTMLChunks () {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
 
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
-};
-https.get(requestOptions, function (response) {
+  https.get(requestOptions, function (response) {
 
-  response.setEncoding('utf8');
+    response.setEncoding('utf8');
 
-  response.on('data', function (data) {
-    console.log('Chunk Received. Length:', data.length);
-    console.log(data + "\n");
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      console.log(data + "\n");
+      });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
     });
-
-  response.on('end', function() {
-    console.log('Response stream complete.');
   });
-
-});
+};
+console.log(getAndPrintHTMLChunks())
